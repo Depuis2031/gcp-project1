7ffb9d802a14e90040b28002f27d62f441b0b1f2
Depuis2031 <lukenjang@gmail.com>
2024-05-06 15:26:06 +0200

    first

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance" "default" {
+  name         = "terraform-instance"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "Ubuntu 20.04 LTS"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "admin:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
---
+++ variable.tf
@@ -0,0 +1,8 @@
+
+variable "project_id" {}
+variable "region" {
+  default = "europe-west9"
+}
+variable "zone" {
+  default = "europe-west9-a "
+}
