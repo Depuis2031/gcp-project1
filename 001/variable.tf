c295876cf5fa7b053902482f61867426d5ba1e44
Depuis2031 <lukenjang@gmail.com>
2024-05-07 11:43:45 +0200

    001 Updated

--- 001/variable.tf
+++ 001/variable.tf
@@ -2,9 +2,9 @@
 variable "region" {
   default = "europe-west9"
 }
-variable "zone" {
-  default = "europe-west9-a"
-}
+#variable "zone" {
+ # default = "europe-west9-a"
+#}
 variable "project_id" {
    type        = string
   default      = "advance-terraform-406410"
