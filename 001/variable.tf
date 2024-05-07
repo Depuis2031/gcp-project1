ba615cc6f04982b75a0a2c4444180d510bb8673d
Depuis2031 <lukenjang@gmail.com>
2024-05-07 11:22:49 +0200

    variable.tf updated

--- 001/variable.tf
+++ 001/variable.tf
@@ -4,10 +4,10 @@ variable "region" {
   default = "europe-west9"
 }
 variable "zone" {
-  default = "europe-west9-a "
+  default = "europe-west9-a"
 }
 variable "project_id" {
    type        = string
   default      = "advance-terraform-406410"
-  description = Google Cloud Project ID
+  description  = #Google Cloud Project ID
 }
